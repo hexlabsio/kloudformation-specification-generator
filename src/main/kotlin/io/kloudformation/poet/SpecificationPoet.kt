27a3396f76ebea3826823e41e89925ec9adf37c8
Gerard Downes <gdownes@gmail.com>
2018-08-01 20:46:53 +0100

    Cleaning type name retrieval

--- src/main/kotlin/io/kloudformation/poet/SpecificationPoet.kt
+++ src/main/kotlin/io/kloudformation/poet/SpecificationPoet.kt
@@ -8,58 +8,60 @@ import java.io.File
 
 object SpecificationPoet {
     fun generate(specification: Specification) = specification.let {
-        it.resourceTypes.map{ buildFile(true, it) }
+        it.resourceTypes
+                .map { buildFile((specification.propertyTypes + specification.resourceTypes).keys, true, it.key, it.value) }
                 .forEach { it.writeTo(File(System.getProperty("user.dir") + "/target/generated-sources")) }
-        it.propertyTypes.map{ buildFile(false, it) }
+        it.propertyTypes
+                .map { buildFile((specification.propertyTypes + specification.resourceTypes).keys, false, it.key, it.value) }
                 .forEach { it.writeTo(File(System.getProperty("user.dir") + "/target/generated-sources")) }
     }
 
-    private fun buildFile(isResource: Boolean, it: Map.Entry<String, PropertyInfo>) =
-            FileSpec.builder(getPackageName(isResource, it.key), getClassName(it))
-                    .addStaticImport("io.kloudformation.property","*")
-                    .addStaticImport("com.fasterxml.jackson.databind", "JsonNode")
-                    .addStaticImport(getPackageName(false, it.key), "*")
-                    .addType(buildType(it))
+    private fun buildFile(types: Set<String>, isResource: Boolean, typeName: String, propertyInfo: PropertyInfo) =
+            FileSpec.builder(getPackageName(isResource, typeName), getClassName(typeName))
+                    .addType(buildType(types, typeName, propertyInfo))
                     .build()
 
-    private fun buildType(it: Map.Entry<String, PropertyInfo>) =
-            TypeSpec.classBuilder(getClassName(it))
-                    .addModifiers(if(!it.value.properties.isEmpty()) KModifier.DATA else KModifier.PUBLIC)
-                    .addKdoc(it.value.documentation)
-                    .primaryConstructor(if(!it.value.properties.isEmpty()) buildFunction(it) else null)
-                    .addProperties(it.value.properties.map(this::buildProperty))
+    private fun buildType(types: Set<String>, typeName: String, propertyInfo: PropertyInfo) =
+            TypeSpec.classBuilder(getClassName(typeName))
+                    .addModifiers(if (!propertyInfo.properties.isEmpty()) KModifier.DATA else KModifier.PUBLIC)
+                    .primaryConstructor(if (!propertyInfo.properties.isEmpty()) buildFunction(types, typeName, propertyInfo) else null)
+                    .addProperties(propertyInfo.properties.map { buildProperty(types, typeName, it.key, it.value) })
                     .build()
 
-    private fun buildFunction(it: Map.Entry<String, PropertyInfo>) =
+    private fun buildFunction(types: Set<String>, classTypeName: String, propertyInfo: PropertyInfo) =
             FunSpec.constructorBuilder()
-                    .addParameters(it.value.properties.map(this::buildParameter))
+                    .addParameters(propertyInfo.properties.map { buildParameter(types, classTypeName, it.key, it.value) })
                     .build()
 
-    private fun buildProperty(it: Map.Entry<String, Property>) =
+    private fun buildProperty(types: Set<String>, classTypeName: String, propertyName: String, property: Property) =
             PropertySpec.builder(
-                    it.key.decapitalize(),
-                    if (it.value.required) getTypeName(it).asNonNullable() else getTypeName(it).asNullable())
+                    propertyName.decapitalize(),
+                    if (property.required) getType(types, classTypeName, property).asNonNullable() else getType(types, classTypeName, property).asNullable())
                     .addModifiers()
-                    .initializer(it.key.decapitalize())
-                    .addKdoc(it.value.documentation)
+                    .initializer(propertyName.decapitalize())
                     .build()
 
-    private fun buildParameter(it: Map.Entry<String, Property>) =
+    private fun buildParameter(types: Set<String>, classTypeName: String, parameterName: String, property: Property) =
             ParameterSpec.builder(
-                    it.key.decapitalize(),
-                    if (it.value.required) getTypeName(it).asNonNullable() else getTypeName(it).asNullable())
+                    parameterName.decapitalize(),
+                    if (property.required) getType(types, classTypeName, property).asNonNullable() else getType(types, classTypeName, property).asNullable())
                     .build()
 
-    private fun getClassName(it: Map.Entry<String, PropertyInfo>) =
-            it.key.split("::", ".").last()
+    private fun getClassName(typeName: String) =
+            typeName.split("::", ".").last()
 
-    private fun getPackageName(isResource: Boolean, key: String) =
-            "io.kloudformation.${if (isResource) "resource" else "property"}${key.split("::", ".").dropLast(1).joinToString(".").toLowerCase().replaceFirst("aws.", ".")}"
+    private fun getPackageName(isResource: Boolean, typeName: String) =
+            "io.kloudformation.${if (isResource) "resource" else "property"}${typeName.split("::", ".").dropLast(1).joinToString(".").toLowerCase().replaceFirst("aws.", ".")}"
 
-    private fun getTypeName(it: Map.Entry<String, Property>) = when {
-        !it.value.primitiveType.isNullOrEmpty() -> ClassName.bestGuess(it.value.primitiveType.toString().replace("Json", "JsonNode").replace("Timestamp", "java.time.Instant").replace("Integer", "kotlin.Int"))
-        !it.value.primitiveItemType.isNullOrEmpty() -> if (it.value.type.equals("Map")) ParameterizedTypeName.get(Map::class.asClassName(), String::class.asClassName(), ClassName.bestGuess(it.value.primitiveItemType.toString())) else ParameterizedTypeName.get(List::class, String::class)
-        !it.value.itemType.isNullOrEmpty() -> ParameterizedTypeName.get(List::class.asClassName(), ClassName.bestGuess(it.value.itemType.toString()))
-        else -> ClassName.bestGuess(it.value.type.toString())
+    private fun getType(types: Set<String>, classTypeName: String, property: Property) = when {
+        !property.primitiveType.isNullOrEmpty() -> ClassName.bestGuess(property.primitiveType.toString().replace("Json", "com.fasterxml.jackson.databind.JsonNode").replace("Timestamp", "java.time.Instant").replace("Integer", "kotlin.Int"))
+        !property.primitiveItemType.isNullOrEmpty() -> if (property.type.equals("Map")) ParameterizedTypeName.get(Map::class.asClassName(), String::class.asClassName(), ClassName.bestGuess(property.primitiveItemType.toString())) else ParameterizedTypeName.get(List::class, String::class)
+        !property.itemType.isNullOrEmpty() -> ParameterizedTypeName.get(List::class.asClassName(), ClassName.bestGuess(getPackageName(false, getTypeName(types, classTypeName, property.itemType.toString())) + "." + property.itemType))
+        else -> ClassName.bestGuess(getPackageName(false, getTypeName(types, classTypeName, property.type.toString())) + "." + property.type)
     }
+
+    private fun getTypeName(types: Set<String>, classTypeName: String, propertyType: String) =
+            types.filter { it == propertyType || it.endsWith(".$propertyType") }.let {
+                if (it.size > 1) it.first { it.contains(classTypeName.split("::").last().split(".").first()) } else it.first()
+            }
 }
