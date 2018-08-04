80ec6d18a3f9fff2ebbfda1e2aba172c4c7a8b1a
Chris Barbour <xbobjamin@gmail.com>
2018-08-04 21:48:07 +0100

    added readme

--- src/test/kotlin/io/kloudformation/merger/SpecificationMergerTest.kt
+++ src/test/kotlin/io/kloudformation/merger/SpecificationMergerTest.kt
@@ -60,12 +60,12 @@ class SpecificationMergerTest {
     @Test
     fun go() {
         val template = KloudFormationTemplate.create {
-            val quadTopic = topic("QuadNotificationTopic")
-            val quadNotificationEndpoint = parameter<String>("QuadNotificationEndpoint", "AWS::EC2::Instance::Id")
-            subscription("QuadNotificationSubscription") {
-                topicArn(quadTopic.ref())
+            val topic = topic("NotificationTopic")
+            val notificationEmail = parameter<String>("EmailAddress")
+            subscription("NotificationSubscription") {
+                topicArn(topic.ref())
                 protocol("email")
-                endpoint(quadNotificationEndpoint + quadTopic.TopicName())
+                endpoint(notificationEmail.ref())
             }
         }
 
@@ -77,28 +77,22 @@ class SpecificationMergerTest {
         )
     }
 
-    //Results in ************************************************************************
-    //---
-    //AWSTemplateFormatVersion: "2010-09-09"
-    //Description: ""
-    //Parameters:
-    //  QuadNotificationEndpoint:
-    //    Type: "AWS::EC2::Instance::Id"
-    //Resources:
-    //  QuadNotificationTopic:
-    //    Type: "AWS::SNS::Topic"
-    //  QuadNotificationSubscription:
-    //    Type: "AWS::SNS::Subscription"
-    //    Endpoint:
-    //      Fn::Join:
-    //      - ""
-    //      - - Ref: "QuadNotificationEndpoint"
-    //        - Fn::GetAtt:
-    //          - "QuadNotificationTopic"
-    //          - "TopicName"
-    //    Protocol: "email"
-    //    TopicArn:
-    //      Ref: "QuadNotificationTopic"
+//---
+//AWSTemplateFormatVersion: "2010-09-09"
+//Description: ""
+//Parameters:
+//  EmailAddress:
+//    Type: "String"
+//Resources:
+//  NotificationTopic:
+//    Type: "AWS::SNS::Topic"
+//  NotificationSubscription:
+//    Type: "AWS::SNS::Subscription"
+//    Endpoint:
+//      Ref: "EmailAddress"
+//    Protocol: "email"
+//    TopicArn:
+//      Ref: "NotificationTopic"
 
 }
 
