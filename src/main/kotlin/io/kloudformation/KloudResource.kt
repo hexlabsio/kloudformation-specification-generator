f9566b0fc232508afe3becc331b377575d272dc6
Chris Barbour <xbobjamin@gmail.com>
2018-08-10 16:19:49 +0100

    made resource type changable to support Custom::String for resource type, closes #12 (Custom resource type already exists

--- src/main/kotlin/io/kloudformation/KloudResource.kt
+++ src/main/kotlin/io/kloudformation/KloudResource.kt
@@ -3,11 +3,10 @@ package io.kloudformation
 import com.fasterxml.jackson.annotation.JsonIgnore
 import io.kloudformation.function.Reference
 import io.kloudformation.function.plus
-import io.kloudformation.model.KloudFormationTemplate
 
 open class KloudResource<T>(
         @JsonIgnore open val logicalName: String,
-        @JsonIgnore open val kloudResourceType: String = "AWS::CustomResource",
+        @JsonIgnore open var kloudResourceType: String = "AWS::CustomResource",
         @JsonIgnore open val dependsOn: List<String>? = null,
         @JsonIgnore open val condition: String? = null
 ){
@@ -19,7 +18,7 @@ open class KloudResource<T>(
     operator fun <R> plus(other: Value<R>) = this.ref() + other
     operator fun <R> plus(other: KloudResource<R>) = this.ref() + other.ref()
 
-
+    fun asCustomResource(resourceType: String) = also { kloudResourceType = "Custom::$resourceType" }
 }
 
 infix fun KloudResource<*>.and(b: KloudResource<*>) = listOf(this, b)
