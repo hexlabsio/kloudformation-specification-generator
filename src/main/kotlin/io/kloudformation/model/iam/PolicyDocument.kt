1c89411065993b3258ca6ad2343fd48c7093612d
Chris Barbour <xbobjamin@gmail.com>
2018-08-27 17:19:48 +0100

    setting version in policydocument to string so that output is correct

--- src/main/kotlin/io/kloudformation/model/iam/PolicyDocument.kt
+++ src/main/kotlin/io/kloudformation/model/iam/PolicyDocument.kt
@@ -7,9 +7,9 @@ import io.kloudformation.model.KloudFormationTemplate
 data class PolicyDocument(
         val statement: List<PolicyStatement>,
         val id: String? = null,
-        val version: IamPolicyVersion? = null
+        val version: String? = null
 ): Value<JsonNode>{
-    data class Builder(val id: String? = null, val version: IamPolicyVersion? = null, val statement: MutableList<PolicyStatement> = mutableListOf()){
+    data class Builder(val id: String? = null, val version: String? = null, val statement: MutableList<PolicyStatement> = mutableListOf()){
         fun statement( action: Action, effect:IamPolicyEffect = IamPolicyEffect.Allow, resource: Resource? = null, sid: String? = null, builder: PolicyStatement.Builder.() -> PolicyStatement.Builder = { this }) = also {
             statement.add(PolicyStatement.create(effect = effect, action = action, resource = resource, sid = sid).builder().build())
         }
@@ -18,8 +18,8 @@ data class PolicyDocument(
     }
 
     companion object {
-        fun create(id: String? = null, version: IamPolicyVersion? = null) = Builder(id, version)
+        fun create(id: String? = null, version: String? = null) = Builder(id, version)
     }
 }
 
-fun KloudFormationTemplate.Builder.policyDocument(id: String? = null, version: IamPolicyVersion? = null, builder: PolicyDocument.Builder.() -> PolicyDocument.Builder) = PolicyDocument.create(id, version).builder().build()
+fun KloudFormationTemplate.Builder.policyDocument(id: String? = null, version: String? = null, builder: PolicyDocument.Builder.() -> PolicyDocument.Builder) = PolicyDocument.create(id, version).builder().build()
