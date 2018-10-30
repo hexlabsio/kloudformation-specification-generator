112da82c4940e83a596a210803aabed2cfa65f10
Chris Barbour <chris.barbour03@gmail.com>
2018-10-30 20:55:07 +0000

    added condition to output

--- src/main/kotlin/io/kloudformation/model/Output.kt
+++ src/main/kotlin/io/kloudformation/model/Output.kt
@@ -2,6 +2,6 @@ package io.kloudformation.model
 
 import io.kloudformation.Value
 
-data class Output(val value: Value<*>, val description: String? = null, val export: Export? = null) {
+data class Output(val value: Value<*>, val description: String? = null, val condition: String? = null, val export: Export? = null) {
     data class Export(val name: Value<String>)
 }
